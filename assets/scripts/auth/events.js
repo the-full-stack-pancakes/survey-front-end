7954caac6bc7047935370d8d5b3ac10f138cf315
binnsyGA <alice.binns17@gmail.com>
2019-05-14 12:23:31 -0400

    Added and checked working curl scripts for auth on local port!

--- assets/scripts/auth/events.js
+++ assets/scripts/auth/events.js
@@ -40,12 +40,29 @@ const onSignOut = function (event) {
     .catch(ui.signOutFailure)
 }
 
-
-
+const signUpToggle = function () {
+  $('#sign-in-modal').hide()
+  $('#sign-up-modal').show()
+}
+const signInToggle = function () {
+  $('#sign-up-modal').hide()
+  $('#sign-in-modal').show()
+}
+const changePasswordToggle = () => {
+  $('#change-password-modal').show()
+}
+const closeChangePassword = () => {
+  $('#change-password-modal').hide()
+}
 
 const authHandlers = () => {
-  // $('.page-mask').show()
-  // $('#sign-in-modal').show()
+  $('.page-mask').show()
+  $('#sign-in-modal').show()
+  $('#sign-up-toggle').on('click', signUpToggle)
+  $('#sign-in-toggle').on('click', signInToggle)
+  $('#change-password-modal').hide()
+  $('#change-password-toggle').on('click', changePasswordToggle)
+  $('.close-change-password').on('click', closeChangePassword)
   //
   // $('#sign-up').on('submit', onSignUp)
   // $('#sign-in').on('submit', onSignIn)
