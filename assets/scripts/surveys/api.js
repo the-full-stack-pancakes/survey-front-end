f13e886bd273f165f8cdd7c4ee6f3ab2ce1ff7c0
ejparnell <ejparnell@hotmail.com>
2019-05-15 07:28:57 -0400

    Add ajax calls for survey
    -Added ajax calls for create, update, delete, and get.
    -Added event listeners and forms so we can end the right information
    to the server.
    -Added event handlers to app.js so button clicks would be successful.

--- assets/scripts/surveys/api.js
+++ assets/scripts/surveys/api.js
@@ -3,18 +3,23 @@ const store = require('../store')
 
 // Same as updateGame
 const createSurvey = function (data) {
-  // console.log(data)
   return $.ajax({
-    url: config.apiUrl + '/surveys/',
+    url: config.apiUrl + '/surveys',
     method: 'POST',
     headers: {
       Authorization: 'Token token=' + store.user.token
     },
-    data
+    data: {
+      survey: {
+        title: data.survey.title,
+        description: data.survey.description,
+        questions: store.question._id
+      }
+    }
   })
 }
 
-const getSurveys = function () {
+const getYourSurveys = function () {
   return $.ajax({
     url: config.apiUrl + '/surveys',
     method: 'GET',
@@ -26,6 +31,8 @@ const getSurveys = function () {
 
 const deleteSurvey = function (id) {
   return $.ajax({
+    // this is going to need a way to get an id of an answer so we can delete
+    // a single id.
     url: config.apiUrl + `/surveys/${id}`,
     method: 'DELETE',
     headers: {
@@ -36,18 +43,28 @@ const deleteSurvey = function (id) {
 
 const updateSurvey = function (data, id) {
   return $.ajax({
+    // this is going to need a way to get an id of an answer so we can update
+    // a single id.
     url: config.apiUrl + `/surveys/${id}`,
     method: 'PATCH',
     headers: {
       Authorization: 'Token token=' + store.user.token
     },
-    data
+    data: {
+      survey: {
+        title: data.survey.title,
+        description: data.survey.description
+        // need to find a way to update when you want a new question.
+        // might have to tie in the update on question api somehow
+        // questions: store.question._id
+      }
+    }
   })
 }
 
 module.exports = {
   createSurvey,
-  getSurveys,
+  getYourSurveys,
   deleteSurvey,
   updateSurvey
 }
--- assets/scripts/surveys/events.js
+++ assets/scripts/surveys/events.js
@@ -1,64 +1,43 @@
-const getFormFields =
-require('./../../../lib/get-form-fields.js')
-const api = require('./api')
-const ui = require('./ui')
-const store = require('../store')
+const api = require('./api.js')
+const ui = require('./ui.js')
+const getFormFields = require('../../../lib/get-form-fields.js')
 
-const onCreateSurvey = function (event) {
+const onCreateSurvey = (event) => {
   event.preventDefault()
   const data = getFormFields(event.target)
   console.log(data)
-  //
-  // api.createSurvey(data)
-  //   .then(ui.createSurveySuccess)
-  //   .catch(ui.createSurveyFailure)
+  api.createSurvey(data)
+    .then(ui.onCreateSurveySuccess)
+    .catch(ui.failure)
 }
-
-const onUpdateSurvey = function (event) {
+const onGetYourSurveys = () => {
+  api.getYourSurveys()
+    .then(ui.onGetYourSurveysSuccess)
+    .catch(ui.failure)
+}
+const onUpdateSurvey = (event) => {
   event.preventDefault()
-
-  // api.updateSurvey(data, id)
-  //   .then(ui.updateSurveySuccess)
-  //   .catch(ui.updateSurveyFailure)
+  const data = getFormFields(event.target)
+  console.log(data)
+  // needs to get an id somehow
+  api.updateSurvey(data)
+    .then(ui.onUpdateSurveySuccess)
+    .catch(ui.failure)
+}
+const onDeleteSurvey = (id) => {
+  // needs to get an id somehow
+  api.deleteSurvey(id)
+    .then(ui.onDeleteSurveySuccess)
+    .catch(ui.failure)
 }
 
-const onGetSurveys = function (event) {
-  event.preventDefault()
-
-  // .then(ui.getSurveysSuccess)
-  // .catch(ui.failure)
-}
-
-const onGetMySurveys = function (event) {
-  event.preventDefault()
-
-}
-
-const onDeleteSurvey = (event) => {
-  event.preventDefault()
-
-  // const id = $(event.target).data('id')
-  // api.deleteSurvey(id)
-  //   .then()
-  //   .catch(ui.failure)
-}
-
-const onClearSurveys = (event) => {
-  // event.preventDefault()
-  // ui.clearSurveys()
-}
-
-const addHandlers = function () {
-  $('#see-all-surveys').on('submit', onGetSurveys)
-  $('#survey-submit').on('submit', onCreateSurvey)
-  $('#create-survey').on('click', onCreateSurvey)
-
-  // $('#getSurveysButton').on('click', onGetSurveys)
-  // $('#clearSurveysButton').on('click', onClearSurveys)
-  // $('#content').on('click', '.delete', onDeleteSurvey)
-  // $('#content').on('submit', '.update-form', onUpdateSurvey)
+const surveyHandlers = function () {
+  $('#create-survey').on('submit', onCreateSurvey)
+  $('#get-surveys').on('click', onGetYourSurveys)
+  $('#update-survey').on('submit', onUpdateSurvey)
+  $('#delete-survey').on('click', onDeleteSurvey)
 }
 
 module.exports = {
-  addHandlers
+  surveyHandlers
 }
