ec1ad7d9bba3fe74c77e7dd726b3cc7c0ee65918
ejparnell <ejparnell@hotmail.com>
2019-05-15 17:44:59 -0400

    Update button works
    -Added data-one and data-two to handlebars so I could target the id
    of answer one and two.
    -Used store so I could reference those ids else where.
    -tested and passes

--- assets/scripts/surveys/events.js
+++ assets/scripts/surveys/events.js
@@ -1,6 +1,25 @@
 const api = require('./api.js')
 const ui = require('./ui.js')
 const getFormFields = require('../../../lib/get-form-fields.js')
+const store = require('../store.js')
+const one = require('../ones/events.js')
+const two = require('../twos/events.js')
+
+const update = (event) => {
+  event.preventDefault()
+  const data = getFormFields(event.target)
+  const id = $(event.target).closest('form').data('id')
+  const oneId = $(event.target).closest('form').data('one')
+  const twoId = $(event.target).closest('form').data('two')
+  console.log('===========================')
+  store.updateSurvey = id
+  store.updateOne = oneId
+  store.updateTwo = twoId
+  console.log('===========================')
+  one.onUpdateOne(data.survey.answer)
+  two.onUpdateTwo(data.survey.answer2)
+  onUpdateSurvey(data.survey.title)
+}
 
 const onCreateSurvey = (event) => {
   api.createSurvey()
@@ -12,11 +31,7 @@ const onGetYourSurveys = () => {
     .then(ui.onGetYourSurveysSuccess)
     .catch(ui.failure)
 }
-const onUpdateSurvey = (event) => {
-  event.preventDefault()
-  const data = getFormFields(event.target)
-  console.log(data)
-  // needs to get an id somehow
+const onUpdateSurvey = (data) => {
   api.updateSurvey(data)
     .then(ui.onUpdateSurveySuccess)
     .catch(ui.failure)
@@ -30,7 +45,7 @@ const onDeleteSurvey = (id) => {
 
 const surveyHandlers = function () {
   $('#get-surveys').on('click', onGetYourSurveys)
-  $("#my-survey-content").on("submit", ".update-survey", onUpdateSurvey)
+  $("#my-survey-content").on("submit", ".update-survey", update)
   $("#my-survey-content").on("click", ".btn-dark", onUpdateSurvey)
 }
 
